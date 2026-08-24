785af83452226105c8db88066182afc627d40295
うるう <140926546+uruu-github@users.noreply.github.com>
2026-08-25 07:39:11 +0900

    Update cat-image.tsx

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,26 +1,26 @@
 "use client";
-
+ 
 import { useState } from "react";
 import { fetchImage } from "./fetch-image";
 import styles from "./page.module.css";
-
+ 
 type CatImageProps = {
   url: string;
 };
-
+ 
 export function CatImage({ url }: CatImageProps) {
   const [imageUrl, setImageUrl] = useState<string>(url);
-
+ 
   const refreshImage = async () => {
     setImageUrl("");
     const image = await fetchImage();
     setImageUrl(image.url);
   };
-
+ 
   return (
     <div className={styles.page}>
       <button onClick={refreshImage} className={styles.button}>
-        他のにゃんこも見る
+        One more cat!
       </button>
       <div className={styles.frame}>
         {imageUrl && <img src={imageUrl} className={styles.img} />}
